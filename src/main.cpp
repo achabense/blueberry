cde85e860551bc602c99eb7788c25b068fd90de1
achabense <60953653+achabense@users.noreply.github.com>
2025-06-18 15:00:19 +0800

    Oversight

--- src/main.cpp
+++ src/main.cpp
@@ -236,6 +236,7 @@ int main(int, char**) {
         }
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
         SDL_ShowWindow(window); // Guaranteed to be all-black.
     }
 
