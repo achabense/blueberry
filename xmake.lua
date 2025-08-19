820c773fb71db11f1f4ec9dec1470899ece4e331
achabense <60953653+achabense@users.noreply.github.com>
2025-08-19 13:31:56 +0800

    xmake

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,11 @@
 -- https://xmake.io/guide/quick-start.html
+-- xmake config -o build
 -- xmake config -m release
 -- xmake
+-- xmake -r
 -- xmake run
 
+set_xmakever("3.0.0")
 -- set_allowedplats("windows")
 set_allowedarchs("x64")
 set_allowedmodes("debug", "release")
@@ -13,14 +16,15 @@ add_rules("mode.debug", "mode.release")
 -- release-3.2.20
 add_requires("libsdl3 96292a5b464258a2b926e0a3d72f8b98c2a81aa6", {configs = {shared = false}})
 
--- TODO: setup build-dir & compiler-specific flags.
 target("blueberry")
     set_kind("binary")
     if is_plat("windows") then
-        add_rules("win.sdk.application")
+        set_values("windows.subsystem", "windows")
+        add_rules("platform.windows.subsystem")
     end
     set_languages("c++20")
     set_encodings("source:utf-8", "target:utf-8")
+    set_warnings("all", "pedantic") -- "extra"
     add_includedirs("src", "imgui", "imgui/backends")
     add_files("src/*.cpp", "imgui/*.cpp", "imgui/backends/*.cpp")
     add_packages("libsdl3")
